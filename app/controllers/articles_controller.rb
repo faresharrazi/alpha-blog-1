a227ae2edaebe3be68d5acf82ace1e8a2b7913f4
Fares <m.fares.harrazi@gmail.com>
2024-11-22 16:13:06 +0200

    add form to create an article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,15 @@ class ArticlesController < ApplicationController
   def index 
     @articles = Article.all
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    byebug
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article.save
+    redirect_to @article
+  end
 end
